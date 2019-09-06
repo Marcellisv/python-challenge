821f915cfbf11af7a0080d380041a9b7e1562975
Daewon Kwon <sliwet@gmail.com>
2019-09-05 22:37:03 -0400

    Upload after fixing a bug

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,29 +7,36 @@ maxinc = 0
 maxincdate = ""
 maxdec = 0
 maxdeccdate = ""
+previous = 0.0
+avgchange = 0
     
 csvpath = os.path.join('budget_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader,None) 
-    
+
     for row in csvreader:
+        current = float(row[1])
         if nmonths == 0:
-            maxinc = float(row[1])
-            maxdec = float(row[1])
+            maxinc = 0.0
+            maxdec = 0.0
             maxincdate = row[0]
             maxdecdate = row[0]
         else:
-            if float(row[1]) > maxinc:
-                maxinc = float(row[1])
+            delta = current - previous
+            avgchange += delta
+            if delta > maxinc:
+                maxinc = delta
                 maxincdate = row[0]
-            elif float(row[1]) < maxdec:
-                maxdec = float(row[1])
+            elif delta < maxdec:
+                maxdec = delta
                 maxdecdate = row[0]
+
+        previous = current
         nmonths += 1
         totalchange += float(row[1])
 
-avgchange = totalchange / nmonths
+avgchange = avgchange / (nmonths-1)
 
 results = []
 results.append("Financial Analysis\n----------------------------")
